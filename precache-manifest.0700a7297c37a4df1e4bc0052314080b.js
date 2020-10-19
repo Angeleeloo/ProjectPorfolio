5d34451e7f0b2b5946fe4e78ff5f81b9a10c8ee4
Angeleeloo <30836556+Angeleeloo@users.noreply.github.com>
2020-10-19 22:30:23 +0300

    Updates

--- precache-manifest.0700a7297c37a4df1e4bc0052314080b.js
+++ precache-manifest.0700a7297c37a4df1e4bc0052314080b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adbf3b201aca28b64aa9c8186a751728",
+    "revision": "aa6011f147dfbe8034ff8c762ce9a5df",
     "url": "/ProjectPorfolio/index.html"
   },
   {
-    "revision": "2cdb0dadc0cdbe5192b9",
+    "revision": "14063e6ac9ff32447f82",
     "url": "/ProjectPorfolio/static/css/main.9370ba3e.chunk.css"
   },
   {
-    "revision": "600cfb608638bfa56f47",
-    "url": "/ProjectPorfolio/static/js/2.9fc7bf2c.chunk.js"
+    "revision": "37bdcd215ab7e96fb149",
+    "url": "/ProjectPorfolio/static/js/2.b96cf776.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/ProjectPorfolio/static/js/2.9fc7bf2c.chunk.js.LICENSE.txt"
+    "url": "/ProjectPorfolio/static/js/2.b96cf776.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2cdb0dadc0cdbe5192b9",
-    "url": "/ProjectPorfolio/static/js/main.144b30f2.chunk.js"
+    "revision": "14063e6ac9ff32447f82",
+    "url": "/ProjectPorfolio/static/js/main.999423fd.chunk.js"
   },
   {
     "revision": "68290db5da77fe4d5d5b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ProjectPorfolio/precache-manifest.560fc826665e7fb569baddf439c8e859.js"
+  "/ProjectPorfolio/precache-manifest.0700a7297c37a4df1e4bc0052314080b.js"
 );
 
 self.addEventListener('message', (event) => {

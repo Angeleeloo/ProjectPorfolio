f51ba5aa55c442c2fdac284b0836a82acb8c0a26
Angeleeloo <30836556+Angeleeloo@users.noreply.github.com>
2020-10-19 15:38:28 +0300

    Updates

---
+++ precache-manifest.560fc826665e7fb569baddf439c8e859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbf3b201aca28b64aa9c8186a751728",
+    "url": "/ProjectPorfolio/index.html"
+  },
+  {
+    "revision": "2cdb0dadc0cdbe5192b9",
+    "url": "/ProjectPorfolio/static/css/main.9370ba3e.chunk.css"
+  },
+  {
+    "revision": "600cfb608638bfa56f47",
+    "url": "/ProjectPorfolio/static/js/2.9fc7bf2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProjectPorfolio/static/js/2.9fc7bf2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cdb0dadc0cdbe5192b9",
+    "url": "/ProjectPorfolio/static/js/main.144b30f2.chunk.js"
+  },
+  {
+    "revision": "68290db5da77fe4d5d5b",
+    "url": "/ProjectPorfolio/static/js/runtime-main.6fdb31c5.js"
+  }
+]);

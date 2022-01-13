aed2183bac1f5908f15c0accec4148b325f5e1b2
Angeleeloo <30836556+Angeleeloo@users.noreply.github.com>
2022-01-13 20:49:39 +0200

    Updates

--- precache-manifest.bc87f993ddfabf8c353cbd23ffcb30f2.js
+++ precache-manifest.bc87f993ddfabf8c353cbd23ffcb30f2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "857acb7735d5ec071ebc55ec6b28b949",
+    "revision": "abfc6a27e0aa2ce9b9d7ad95bd565556",
     "url": "/ProjectPorfolio/index.html"
   },
   {
-    "revision": "b58e490e1c67e87640c6",
+    "revision": "b9b99f5c33dad5e85d55",
     "url": "/ProjectPorfolio/static/css/main.9370ba3e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ProjectPorfolio/static/js/2.b96cf776.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b58e490e1c67e87640c6",
-    "url": "/ProjectPorfolio/static/js/main.99b54a95.chunk.js"
+    "revision": "b9b99f5c33dad5e85d55",
+    "url": "/ProjectPorfolio/static/js/main.1731af5a.chunk.js"
   },
   {
     "revision": "68290db5da77fe4d5d5b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ProjectPorfolio/precache-manifest.14434c6a3fb9f68a8a87fdfb0e7dd3bb.js"
+  "/ProjectPorfolio/precache-manifest.bc87f993ddfabf8c353cbd23ffcb30f2.js"
 );
 
 self.addEventListener('message', (event) => {
